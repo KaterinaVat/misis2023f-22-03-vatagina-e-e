bd9f3dfc72f80bdf3441a580fad8dc7672f7b3d5
KatushkaVatagina <113946233+KaterinaVat@users.noreply.github.com>
2023-11-20 08:18:25 +0300

    Add files via upload

---
+++ prj.codeforces/1899a.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main() {
+    int n;
+    std::cin >> n;
+    int number = 0;
+    while (n >0){
+        std::cin >> number;
+        if (number % 3 == 0) {
+            std::cout << "Second" << std::endl;
+
+        }
+        else {
+            std::cout << "First" << std::endl;
+        }
+
+        n -= 1;
+    }
+}
+
+
+    
+   
+
+
+
+
